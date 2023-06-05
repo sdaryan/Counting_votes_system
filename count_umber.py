9885e1bf18cab541842b8879aa80483a9317acd7
Siavash Kordi Darian <sdaryan@gmail.com>
2023-06-05 12:57:23 +0430

    The code was modified using OrderedDict

--- count_umber.py
+++ count_umber.py
@@ -1,22 +1,18 @@
 #سیستم شماره آراء
+import collections
 
 ara = []
 ray = dict()
 n = int(input("Enter the number of elements: "))
 
-for i in range(0, n):
+for i in range(0, n):   #get Counting of  votes
     ara.append(input())
 
-for i in range(0, len(ara)):
+for i in range(0, len(ara)):         #get votes 
     ray[ara[i]] = ara.count(ara[i])
 
-sorted_keys = sorted(ray.keys())
-sorted_ray = {}
-for key in sorted_keys:
-    sorted_ray[key] = ray[key]
-for k,v in sorted_ray.items():
-    print(k ,' ', v)   
 
-
-
+od = collections.OrderedDict(sorted(ray.items()))  #sorting list
+for key, value in od.items():
+    print(key," : ", value)
 
